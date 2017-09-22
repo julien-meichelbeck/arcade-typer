fe2897987342f3a704c11b873c1b798b014a85b0
Julien Meichelbeck <julien.meichelbeck@gmail.com>
2017-09-22 21:15:00 +0200

    Merge index & App

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,34 @@
 import 'babel-polyfill'
 import React from 'react'
+import { Switch } from 'react-router'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { HOME_ROUTE, gameRoute } from 'shared/routes'
+import Nav from 'client/component/Nav'
+import HomePage from 'client/component/Home'
+import GamePage from 'client/component/Game'
+import NotFoundPage from 'client/component/not-found'
+import Container from 'client/component/Container'
+import account from 'client/account'
+import Rx from 'rxjs'
+import recompact from 'shared/modules/recompact'
 import { render } from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import { BrowserRouter } from 'react-router-dom'
-import App from 'client/App'
 import { APP_CONTAINER_SELECTOR } from 'shared/config'
 
-render(
+const App = recompact.withObs(() => ({
+  currentAccount$: new Rx.BehaviorSubject(account),
+}))(() => (
   <BrowserRouter>
-    <AppContainer>
-      <App />
-    </AppContainer>
-  </BrowserRouter>,
-  document.querySelector(APP_CONTAINER_SELECTOR),
-)
+    <div>
+      <Nav />
+      <Container>
+        <Switch>
+          <Route path={gameRoute()} render={props => <GamePage gameId={props.match.params.gameId} />} />
+          <Route exact path={HOME_ROUTE} render={() => <HomePage />} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Container>
+    </div>
+  </BrowserRouter>
+))
+
+render(<App />, document.querySelector(APP_CONTAINER_SELECTOR))
